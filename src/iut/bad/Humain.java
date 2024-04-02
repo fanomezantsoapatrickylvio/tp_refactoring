4320ed2bf0435fdd56543e813727142d4bb93c2a
Sylvio Hayler <fanomezantsoapatricksylvio@gmail.com>
2024-04-02 21:43:44 +0300

    affiche nom prenom age

--- src/iut/bad/Humain.java
+++ src/iut/bad/Humain.java
@@ -5,5 +5,10 @@ public class Humain {
 	protected String nom;
 	protected String prenom;
 	protected int age; 
+	
+	public void details()
+	{
+		System.out.println("Nom "+nom+" Prenom "+prenom+" et age"+age);
+	}
 
 }
